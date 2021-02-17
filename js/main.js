c11a260e04be1e481ce1bf22969ce10719a6a41a
Aleksandr Parshukov <porshe97@yandex.ru>
2021-02-17 12:02:53 +0300

    Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
-alert(getRandomNumber(10, 20));
+alert(getRandomNumber(2, 3));
 alert(checkLength('123', 4))
 
 function getRandomNumber(min, max) {
-  return Math.round(Math.random() * (max - min) + min);
+  if (min < max) {
+    if (min >= 0) {
+      return Math.round(Math.random() * (max - min) + min);
+    }
+    else {
+      throw 'Numbers must be greater than or equal to zero';
+    }
+  }
+  else {
+    throw 'The first number must be less than the second';
+  }
 }
 
 function checkLength (string, maxLength) {
