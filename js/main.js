7d8d6f0e4b93d11fdd38278b6b2f8c3abb1920f2
Aleksandr Parshukov <porshe97@yandex.ru>
2021-02-18 08:07:38 +0300

    Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -2,17 +2,16 @@ alert(getRandomNumber(2, 3));
 alert(checkLength('123', 4))
 
 function getRandomNumber(min, max) {
-  if (min < max) {
-    if (min >= 0) {
-      return Math.round(Math.random() * (max - min) + min);
-    }
-    else {
-      throw 'Numbers must be greater than or equal to zero';
-    }
-  }
-  else {
+
+  if (min >= max) {
     throw 'The first number must be less than the second';
   }
+
+  if (min < 0) {
+    throw 'Numbers must be greater than or equal to zero';
+  }
+
+  return Math.round(Math.random() * (max - min) + min);
 }
 
 function checkLength (string, maxLength) {
