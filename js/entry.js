80b991c4f3239e1fbdbac34d5f7c6275ab3a80ac
Aleksandr Parshukov <porshe97@yandex.ru>
2021-03-12 11:27:41 +0300

    Отрисуй меня полностью (часть 1)

--- js/entry.js
+++ js/entry.js
@@ -5,14 +5,24 @@ const ENTRIES_VALUE = 25;
 const entriesListElement = document.querySelector('.pictures');
 const entryTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const entriesList = createEntries(ENTRIES_VALUE);
-const entriesListFragment = document.createDocumentFragment();
 
-entriesList.forEach(({url, likes, comments}) => {
+const entriesElementList = entriesList.map(({url, likes, comments}) => {
   const entryElement = entryTemplate.cloneNode(true);
   entryElement.querySelector('.picture__img').setAttribute('src', url);
   entryElement.querySelector('.picture__comments').textContent =  comments.length;
   entryElement.querySelector('.picture__likes').textContent = likes;
-  entriesListFragment.appendChild(entryElement);
+  return entryElement;
 });
 
-entriesListElement.appendChild(entriesListFragment);
+renderEntries(entriesElementList);
+
+
+function renderEntries(entries) {
+  const entriesListFragment = document.createDocumentFragment();
+
+  entries.forEach((value) => {
+    entriesListFragment.appendChild(value);
+  })
+
+  entriesListElement.appendChild(entriesListFragment);
+}
